7d8203c0b4c589f2988fa0fbb9547e01c80c4860
yctw2008 <y_c_2006@yahoo.com.tw>
2017-07-14 20:02:15 +0800

    Add Lecture02 Example1

---
+++ Lecture02/Example01/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example01
+{
+    public class Class1
+    {
+        private static void Main(string[] args)
+        { 
+        Console.Write("請輸入數字1:");
+            int number1 = int.Parse (Console.ReadLine());
+        Console.Write("請輸入數字2:");
+        Console.WriteLine("{0}+{1}={2}");
+               , number1
+               , number2
+               , number1 + number2);
+        }
+}
--- PokemonHM/ClassLibrary1/pokemon.cs
+++ PokemonHM/ClassLibrary1/pokemon.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public class Class1
+    public class pokemon
     {
-    }
+        static void Main(string[] args)
+        {
+            Pokemon aPokemon = new Pokemon()
+            {
+                 name = "妙蛙種子";
+                 hp = "60/60";
+                 gender = "男";
+                 weight kg = "5.94";
+                 type = "草/毒";
+                 height m = "0.67";
+                 Stardust = "271484";
+                 Candy = "27";
+                 strengthen = "3000, 3";
+                 EVO = "25";
+                 skill(1) = "藤編";
+                 skill(01) = "7";
+                 skill(2) = "汙泥炸彈";
+                 skill(02) = "80";
+             };
+             Console.WriteLine(
+                  "名稱:{0}, 強度:{1}, 性別:{2}, 體重:{3}, 屬性:{4}, 身高:{5}, 星星沙子:{6}, 妙娃種子的糖果:{7}, 強化:{8}{9}, 進化:{10}, 技能(01):{11}, 技能(02):{12}"
+                 , aPokemon.name 
+                 , aPokemon.hp 
+                 , aPokemon.gender 
+                 , aPokemon.weight 
+                 , aPokemon.type 
+                 , aPokemon.height 
+                 , aPokemon.stardust 
+                 , aPokemon.candy 
+                 , aPokemon.strengthen 
+                 , aPokemon.EVO 
+                 , aPokemon.skill(1) 
+                 , aPokemon.skill(01)
+                 , aPokemon.skill(2) 
+                 , aPokemon.skill(02)
+             };
+
+
+
+
 }
