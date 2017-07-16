6d16652ed5670c63e3db34a840514f1fc44e0f00
yctw2008 <y_c_2006@yahoo.com.tw>
2017-07-16 14:38:43 +0800

    Add lecture02-Examples

--- Lecture02/Example01/Class1.cs
+++ Lecture02/Example01/Class1.cs
@@ -12,8 +12,8 @@ namespace Example01
         { 
         Console.Write("請輸入數字1:");
             int number1 = int.Parse (Console.ReadLine());
-        Console.Write("請輸入數字2:");
-        Console.WriteLine("{0}+{1}={2}");
+            int number2 = int.Parse (Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}"
                , number1
                , number2
                , number1 + number2);
---
+++ Pokemonlabrary/Pokemon/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    public class Class1
+    {
+        public string Name;
+        public int CurrentHp;
+        public int Hp;
+        public string[] Types;
+        public float Height;
+        public float Weight;
+        public int PowerupStardust;
+        public int PowerupCandy;
+        public int EvolveCandy;
+    }
+}
