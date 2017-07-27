7714028fda6e78b75c885e3b29d1a70f103075e3
yctw2008 <y_c_2006@yahoo.com.tw>
2017-07-27 16:53:15 +0800

    Add Lecture05 Example1

---
+++ Lecture05-Examples/Lecture05-Example01/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture05_Example01
+{
+    public enum Status
+    {
+        Success =1, Error =404, Disconnect =3, Connected =4;
+    }
+}
