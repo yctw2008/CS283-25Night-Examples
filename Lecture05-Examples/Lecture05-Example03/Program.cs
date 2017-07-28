49f2e5edad703dd9385a7ec83d5ea430e21026a5
yctw2008 <y_c_2006@yahoo.com.tw>
2017-07-28 15:49:49 +0800

    Add Lecture05-Examples

---
+++ Lecture05-Examples/Lecture05-Example03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture05_Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+            for(int next =1; next <=100; next++)
+            {
+                total += next;
+            }
+            Console.WriteLine(total);
+
+        }
+    }
+}
