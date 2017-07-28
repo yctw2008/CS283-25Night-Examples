5c9a6bad8a0c70b10f4a25aa3c376df5f7bcf3f2
yctw2008 <y_c_2006@yahoo.com.tw>
2017-07-28 16:24:32 +0800

    Add Lecture05-Examples

---
+++ Lecture05-Examples/Lecture05-Example06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture05_Example06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int next = 0;
+            int total = 0;
+            while (total < 1000)
+            {
+                total += (next++);
+            }
+
+            Console.WriteLine("next =" + next);
+            Console.WriteLine("total =" + total)
+            
+        }
+    }
+}
